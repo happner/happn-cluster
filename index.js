223c9237a6717703873a9919984939419da3cb15
nomilous <nomilous@gmail.com>
2016-09-18 19:37:14 +0200

    cluster class

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require('./lib/happn-cluster');
--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var filename = path.basename(__filename);
 
+var HappnCluster = require('../');
+
 describe(filename, function() {
   it('tests', function(done) {
     done();
