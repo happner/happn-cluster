0b4a825aca172a6fb2416114fef1117a3b2d6d33
Grant Pidwell <grantpidwell@gmail.com>
2016-09-27 09:14:09 +0200

    Modified proxy to use this.happn. Added mock-happn to tests; modified tests.

---
+++ test/mocks/mock-happn.js
@@ -0,0 +1,45 @@
+/**
+ * Created by grant on 2016/09/27.
+ */
+
+var path = require('path');
+
+var MockHappn = function (mode) {
+  this.__mode = 'mode';
+};
+
+Object.defineProperty(MockHappn.prototype, "log", {
+  get: function () {
+    return {
+      error: function (message, err) {
+        console.log(message);
+        throw(err);
+      }
+    }
+  }
+});
+
+Object.defineProperty(MockHappn.prototype, "config", {
+  get: function () {
+    return {
+      transport: {
+        mode: this.__mode
+      }
+    }
+  }
+});
+
+Object.defineProperty(MockHappn.prototype, "server", {
+  get: function () {
+    return {
+      address: function () {
+        return {
+          address: '0.0.0.0',
+          port: 9000
+        }
+      }
+    }
+  }
+});
+
+module.exports = MockHappn;
--- test/unit-proxy.js
+++ test/unit-proxy.js
@@ -5,6 +5,7 @@
 var path = require('path');
 var filename = path.basename(__filename);
 var Proxy = require('../lib/proxy');
+var MockHappn = require('./mocks/mock-happn');
 
 describe(filename, function () {
 
@@ -14,24 +15,25 @@ describe(filename, function () {
 
   before('sets up configuration', function (done) {
 
+    var self = this;
+
     /*
-     protocol: 'ws','tcp','http','https'
-     targetHost: '127.0.0.1'
-     targetPort:'9000'
+     listenHost:'0.0.0.0'
      listenPort:'8015'
-     secure:'true','false'
      key:'asdasd' (only interrogated if secure:true)
      cert:'asdadsasd' (only interrogated if secure:true)
      */
 
-    this.__config = {
-      protocol: 'http',
-      targetHost: '127.0.0.1',
-      targetPort: '9000',
-      listenHost: '127.0.0.1',
-      listenPort: '8015',
-      secure: 'false'
+    self.__config = {
+      listenHost: '0.0.0.0',
+      listenPort: '8015'
     };
+
+    Object.defineProperty(Proxy.prototype, "happn", {
+      get: function () {
+        return new MockHappn('http');
+      }
+    });
 
     done();
   });
@@ -39,6 +41,7 @@ describe(filename, function () {
   it('can initialize the proxy', function (done) {
 
     var self = this;
+
     var proxy = new Proxy();
 
     proxy.initialize(self.__config, function (err, result) {
@@ -96,7 +99,8 @@ describe(filename, function () {
       res.end();
     });
 
-    proxiedServer.listen(self.__config.targetPort);
+    // the proxied server is set up as the target in happn (the mock in this case)
+    proxiedServer.listen(proxy.happn.server.address().port);
 
     // set up proxy
     proxy.initialize(self.__config, function (err, result) {
@@ -110,26 +114,26 @@ describe(filename, function () {
         // send GET request to proxy - this should pass the request to the target
         http.request({port: self.__config.listenPort, host: self.__config.listenHost}, function (res) {
 
-          var result = '';
+            var result = '';
 
-          res.on('data', function (chunk) {
-            result += chunk;
-          });
-
-          res.on('end', function () {
-            proxy.stop(function (err) {
-              if (err)
-                return done(err);
-
-              assert.equal(result, EXPECTED);
-
-              return done();
+            res.on('data', function (chunk) {
+              result += chunk;
             });
+
+            res.on('end', function () {
+              proxy.stop(function (err) {
+                if (err)
+                  return done(err);
+
+                assert.equal(result, EXPECTED);
+
+                return done();
+              });
+            })
           })
-        }).end();
+          .end();
       });
     });
   });
-
 });
 
