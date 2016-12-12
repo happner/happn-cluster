605e5b3f43560ffff03575a5152ca66eda8d10fd
nomilous <nomilous@gmail.com>
2016-12-12 12:28:34 +0200

    random activity

--- test/b2-func-random-activity.js
+++ test/b2-func-random-activity.js
@@ -1,10 +1,15 @@
 var path = require('path');
 var filename = path.basename(__filename);
+var Promise = require('bluebird');
+var Happn = require('happn-3');
 var benchmarket = require('benchmarket');
-var RandomActivity = require('happn-random-activity-generator');
 var hooks = require('./lib/hooks');
+var MultiRandomActivity = require('./lib/multi-random-activity');
 
-var clusterSize = 20;
+var clusterSize = 10;
+var happnSecure = true;
+var proxySecure = true;
+var duration = 2000;
 
 describe.only(filename, function () {
 
@@ -17,14 +22,69 @@ describe.only(filename, function () {
   });
 
   hooks.startMultiProcessCluster({
-    size: clusterSize
+    size: clusterSize,
+    happnSecure: happnSecure,
+    proxySecure: proxySecure
+  });
+
+  before('connect a client to each server', function (done) {
+    var _this = this;
+    Promise.resolve(this.__configs).map(function (config) {
+      var loginConfig = {
+        config: {
+          secure: happnSecure,
+          host: config.services.proxy.config.host,
+          port: config.services.proxy.config.port,
+          protocol: 'https',
+          username: config.services.security.config.adminUser.username,
+          password: config.services.security.config.adminUser.password
+        }
+      };
+
+      return Happn.client.create(loginConfig);
+    })
+      .then(function (clients) {
+        clients.forEach(function (client) {
+          client.onAsync = Promise.promisify(client.on);
+        });
+        _this.clients = clients;
+        done();
+      })
+      .catch(done);
+  });
+
+
+  before('create activity generators', function () {
+    this.generator = new MultiRandomActivity(this.clients, {onTimeout: 10000});
   });
 
 
   it('tests random activity', function (done) {
-    done();
+    var generator = this.generator;
+    generator.generateActivityStart('test', function (e) {
+      if (e) return done(e);
+      setTimeout(function () {
+        generator.generateActivityEnd('test', function (log) {
+          generator.verify(function (e, logs) {
+            done(e);
+          }, 'test');
+        });
+      }, duration);
+
+    });
   });
 
+
+  after('disconnect all clients', function (done) {
+    if (!this.clients) return done();
+    Promise.resolve(this.clients).map(function (client) {
+      return client.disconnect();
+    })
+      .then(function () {
+        done();
+      })
+      .catch(done);
+  });
 
   hooks.stopMultiProcessCluster();
 
