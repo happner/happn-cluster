16b732f1a0ed749e9d2784fc806500ce79fff5cd
nomilous <nomilous@gmail.com>
2016-12-22 14:09:50 +0200

    latest swim

--- example/lib/create-config.js
+++ example/lib/create-config.js
@@ -34,7 +34,7 @@ module.exports = function (seq, name) {
           join: 'static',
           seed: seq == 0,
           port: 56000 + seq,
-          hosts: [ipAddress + ':56000' /*, ipAddress + ':56001', ipAddress + ':56002'*/]
+          hosts: [ipAddress + ':56000' , ipAddress + ':56001', ipAddress + ':56002']
         }
       },
       proxy: {
