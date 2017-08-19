86fadd2c8fdd3a60719c900df087b76d0fed416d
nomilous <nomilous@gmail.com>
2017-08-19 15:50:49 +0200

    test cluster peer restart

---
+++ test/test-func/19-func-restart-cluster-peer.js
@@ -0,0 +1,43 @@
+var path = require('path');
+var filename = path.basename(__filename);
+var benchmarket = require('benchmarket');
+var HappnCluster = require('../../');
+
+var hooks = require('../lib/hooks');
+var testSequence = parseInt(filename.split('-')[0]);
+var clusterSize = 5;
+var happnSecure = true;
+
+describe(filename, function () {
+
+  this.timeout(30000);
+
+  benchmarket.start();
+
+  hooks.startCluster({
+    testSequence: testSequence,
+    size: clusterSize,
+    happnSecure: happnSecure
+  });
+
+  hooks.stopCluster();
+
+  it('can restart a cluster peer', function (done) {
+    var _this = this;
+    console.log('\n\n\nSTOP\n\n\n');
+    var server = this.servers.pop();
+    var config = this.__configs[this.__configs.length -1];
+    return server.stop()
+      .then(function () {
+        return HappnCluster.create(config)
+      })
+      .then(function (server) {
+        _this.servers.push(server);
+      })
+      .then(done).catch(done);
+  });
+
+  after(benchmarket.store());
+  benchmarket.stop();
+
+});
