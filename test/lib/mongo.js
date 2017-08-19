769ca3dfff9ddfc28fadd01a3303e2514affc119
nomilous <nomilous@gmail.com>
2017-08-19 15:50:35 +0200

    fix tests

--- test/lib/mongo.js
+++ test/lib/mongo.js
@@ -1,11 +1,49 @@
 var MongoClient = require('mongodb').MongoClient;
 
-module.exports.clearCollection = function (url, collection, callback) {
-  MongoClient.connect(url, function (err, db) {
+module.exports.clearCollection = function (url, collectionName, callback) {
+  // url assumes databaseName is the same collectionName
+  MongoClient.connect(url + '/' + collectionName, function (err, db) {
     if (err) return callback(err);
-    db.collection(collection).deleteMany({}, function (err) {
-      if (err) return callback(err);
-      setTimeout(callback, 600);
+    db.collection(collectionName, function (err, collection) {
+      if (err) {
+        db.close();
+        return callback(err);
+      }
+      collection.drop(function (err) {
+        db.close();
+        if (err && err.message == 'ns not found') {
+          return callback(null); // no such collection to delete
+        }
+        callback(err);
+      });
     });
   });
 };
+
+/*
+
+var MongoClient = require('mongodb').MongoClient;
+
+module.exports = function (url, collectionName, callback) {
+  // url assumes databaseName is the same collectionName
+  MongoClient.connect(url + '/' + collectionName, function (err, db) {
+    if (err) return callback(err);
+    db.collection(collectionName, function (err, collection) {
+      if (err) {
+        db.close();
+        return callback(err);
+      }
+      collection.drop(function (err) {
+        db.close();
+        if (err && err.message == 'ns not found') {
+          return callback(null); // no such collection to delete
+        }
+        callback(err);
+      });
+    });
+  });
+};
+
+
+
+*/
